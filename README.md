bed96650ee4043140ce02bf7c3850104453b3c59
leon <monstermovie@gmail.com>
2019-12-16 16:20:47 +0000

    added a multi room moderator mode, templated html, room announcements, list connected users

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ $ pip install -r requirements.txt
 ```shell
 $ python chat.py
 ```
+
+# Networking
+Be aware there are differnt layers of networking going on in this app when running with docker 
+docker-compose cluster of containers are using the "links" directive in the docker-compose.yml file
+this allows the chat.py script in the 'chat' container to use a hostname 'redis' to connect to the redis container.
+
+the example 
+https://docs.docker.com/compose/gettingstarted/#redis-service
+does not use the 'links' directive , but this didnt work for me
+
+https://docs.docker.com/compose/networking/
+suggests using links 
+
+however 
+https://docs.docker.com/compose/compose-file/compose-file-v2/#links   
+
+says
+"Links are a legacy option. We recommend using networks instead."
+
+Currently Docker's documentation is confusing in this respect 
--- chat.py
+++ chat.py
@@ -3,20 +3,28 @@ import aioredis
 import uvloop
 import socket
 from fastapi import FastAPI, Depends
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 from starlette.websockets import WebSocket, WebSocketDisconnect
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
+from aioredis.errors import ConnectionClosedError as ServerConnectionClosedError
 
-REDIS_HOST = 'localhost'
+REDIS_HOST = 'redis'
 REDIS_PORT = 6379
-XREAD_TIMEOUT = 100000
+XREAD_TIMEOUT = 0
 XREAD_COUNT = 100
 NUM_PREVIOUS = 30
 STREAM_MAX_LEN = 1000
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-app = FastAPI()
+ALLOWED_ROOMS = ['chat:1', 'chat:2', 'chat:3', 'leonfeed']
 PORT = 8080
 HOST = "0.0.0.0"
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 def get_local_ip():
@@ -24,86 +32,88 @@ def get_local_ip():
     copy and paste from
     https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
     """
-    ip = [l for l in (
-        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if
-         not ip.startswith("127.")][:1], [
-            [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s
-             in
-             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][
-        0][
-        0]
+    try:
+        ip = [l for l in (
+            [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if
+             not ip.startswith("127.")][:1], [
+                [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s
+                 in
+                 [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][
+            0][
+            0]
+    except OSError as e:
+        print(e)
+        return '127.0.0.1'
     print(ip)
     return ip
 
 
-html = f"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form name="chat-info" id="chat-info" action="#" onsubmit="connect(event)">
-            user: <input type="text" name="username" value="anon" autocomplete="off"/>
-            room: <input type="text" name="room" value="chat1" autocomplete="off"/>
-            <button>Connect</button>
-        </form>
-        <ul id='messages' style="height: 60vh; overflow: scroll">
-        </ul>
-        <form action=""  style="height: 20vh" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-
-        <script>
-            var ws = null
-            function connect(event) {{
-                event.preventDefault()
-
-                if (ws != null) {{
-                  ws.close()
-                }}
-
-                var room_info = new URLSearchParams(new FormData(document.getElementById('chat-info'))).toString()
-                ws = new WebSocket("ws://{get_local_ip()}:{PORT}/ws?" + room_info)
-
-                ws.onmessage = function(event) {{
-                    var messages = document.getElementById('messages')
-                    var message = document.createElement('li')
-                    var content = document.createTextNode(event.data)
-                    message.appendChild(content)
-                    messages.appendChild(message)
-                    messages.scrollTop = messages.scrollHeight;
-                }};
-            }}
-            function sendMessage(event) {{
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }}
-        </script>
-    </body>
-</html>
-"""
+async def get_redis_pool():
+    try:
+        pool = await aioredis.create_redis_pool(
+            (REDIS_HOST, REDIS_PORT), encoding='utf-8')
+        return pool
+    except ConnectionRefusedError as e:
+        print('cannot connect to redis on:', REDIS_HOST, REDIS_PORT)
+        return None
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+async def get_chat_history():
+    pass
+
+
+async def ws_send_moderator(websocket: WebSocket, chat_info: dict):
+    """
+    wait for new items on chat stream and
+    send data from server to client over a WebSocket
+
+    :param websocket:
+    :type websocket:
+    :param chat_info:
+    :type chat_info:
+    """
+    pool = await get_redis_pool()
+    streams = chat_info['room'].split(',')
+    latest_ids = ['$' for i in streams]
+    ws_connected = True
+    print(streams, latest_ids)
+    while pool and ws_connected:
+        try:
+            events = await pool.xread(
+                streams=streams,
+                count=XREAD_COUNT,
+                timeout=XREAD_TIMEOUT,
+                latest_ids=latest_ids
+            )
+            for _, e_id, e in events:
+                e['e_id'] = e_id
+                await websocket.send_json(e)
+                #latest_ids = [e_id]
+        except ConnectionClosedError:
+            ws_connected = False
+
+        except ConnectionClosedOK:
+            ws_connected = False
 
 
 async def ws_send(websocket: WebSocket, chat_info: dict):
-    pool = await aioredis.create_redis_pool(
-        (REDIS_HOST, REDIS_PORT), encoding='utf-8')
+    """
+    wait for new items on chat stream and
+    send data from server to client over a WebSocket
+
+    :param websocket:
+    :type websocket:
+    :param chat_info:
+    :type chat_info:
+    """
+    pool = await get_redis_pool()
     latest_ids = ['$']
-    connected = True
+    ws_connected = True
     first_run = True
-    while connected:
+    while pool and ws_connected:
         try:
             if first_run:
+                # fetch some previous chat history
                 events = await pool.xrevrange(
                     stream=chat_info['room'],
                     count=NUM_PREVIOUS,
@@ -113,7 +123,8 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                 first_run = False
                 events.reverse()
                 for e_id, e in events:
-                    await websocket.send_text(f"{e_id}, {e}")
+                    e['e_id'] = e_id
+                    await websocket.send_json(e)
             else:
                 events = await pool.xread(
                     streams=[chat_info['room']],
@@ -122,54 +133,187 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                     latest_ids=latest_ids
                 )
                 for _, e_id, e in events:
-                    await websocket.send_text(f"{e_id}, {e}")
+                    e['e_id'] = e_id
+                    await websocket.send_json(e)
                     latest_ids = [e_id]
         except ConnectionClosedError:
-            connected = False
+            ws_connected = False
 
         except ConnectionClosedOK:
-            connected = False
+            ws_connected = False
+
+        except ServerConnectionClosedError:
+            print('redis server connection closed')
+            return
+    pool.close()
 
 
 async def ws_recieve(websocket: WebSocket, chat_info: dict):
-    await announce(chat_info, 'connected')
-    pool = await aioredis.create_redis_pool(
-        (REDIS_HOST, REDIS_PORT), encoding='utf-8')
-    connected = True
-    while connected:
+    """
+    receive json data from client over a WebSocket, add messages onto the
+    associated chat stream
+
+    :param websocket:
+    :type websocket:
+    :param chat_info:
+    :type chat_info:
+    """
+    ws_connected = False
+    pool = await get_redis_pool()
+    added = await add_room_user(chat_info, pool)
+
+    if added:
+        await announce(pool, chat_info, 'connected')
+        ws_connected = True
+    else:
+        print('duplicate user error')
+
+    while ws_connected:
         try:
-            data = await websocket.receive_text()
+            data = await websocket.receive_json()
+            fields = {
+                'uname': chat_info['username'],
+                'msg': data['msg'],
+                'type': 'comment',
+                'room': chat_info['room']
+            }
             await pool.xadd(stream=chat_info['room'],
-                            fields={
-                                'username': chat_info['username'],
-                                'msg': data
-                            },
+                            fields=fields,
                             message_id=b'*',
                             max_len=STREAM_MAX_LEN)
         except WebSocketDisconnect:
-            await announce(chat_info, 'disconnected')
-            connected = False
+            await remove_room_user(chat_info, pool)
+            await announce(pool, chat_info, 'disconnected')
+            ws_connected = False
+
+        except ServerConnectionClosedError:
+            print('redis server connection closed')
+            return
+
+        except ConnectionRefusedError:
+            print('redis server connection closed')
+            return
+
+    pool.close()
 
 
-async def announce(chat_info: dict, action: str):
-    pool = await aioredis.create_redis_pool(
-        (REDIS_HOST, REDIS_PORT), encoding='utf-8')
+async def add_room_user(chat_info: dict, pool):
+    added = await pool.sadd(chat_info['room']+":users", chat_info['username'])
+    return added
+
+
+async def remove_room_user(chat_info: dict, pool):
+    removed = await pool.srem(chat_info['room']+":users", chat_info['username'])
+    return removed
+
+
+async def room_users(chat_info: dict, pool):
+    users = await pool.smembers(chat_info['room']+":users")
+    #print(users)
+    return users
+
+
+async def announce(pool, chat_info: dict, action: str):
+    """
+    add an announcement event onto the redis chat stream
+    """
+    users = await room_users(chat_info, pool)
+    fields = {
+        'msg': f"{chat_info['username']} {action}",
+        'action': action,
+        'type': 'announcement',
+        'users': ", ".join(users),
+        'room': chat_info['room']
+    }
+    #print(fields)
+
     await pool.xadd(stream=chat_info['room'],
-                    fields={'msg': f"{chat_info['username']} {action}"},
+                    fields=fields,
                     message_id=b'*',
                     max_len=STREAM_MAX_LEN)
 
 
 async def chat_info_vars(username: str = None, room: str = None):
+    """
+    URL parameter info needed for a user to participate in a chat
+    :param username:
+    :type username:
+    :param room:
+    :type room:
+    """
     return {"username": username, "room": room}
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket,
                              chat_info: dict = Depends(chat_info_vars)):
+    # check the user is allowed into the chat room
+    verified = await verify_user_for_room(chat_info)
+    # open connection
     await websocket.accept()
-    await asyncio.gather(ws_recieve(websocket, chat_info),
-                         ws_send(websocket, chat_info))
+    if not verified:
+
+        print('failed verification')
+        print(chat_info)
+        await websocket.close()
+    else:
+        # spin up coro's for inbound and outbound communication over the socket
+        await asyncio.gather(ws_recieve(websocket, chat_info),
+                             ws_send(websocket, chat_info))
+
+
+@app.websocket("/ws/moderator")
+async def websocket_moderator_endpoint(websocket: WebSocket,
+                             chat_info: dict = Depends(chat_info_vars)):
+    # check the user is allowed into the chat room
+    # verified = await verify_user_for_room(chat_info)
+    # open connection
+
+    if not chat_info['username'] == 'moderator':
+        print('failed verification')
+        await websocket.close()
+        return
+
+    await websocket.accept()
+    # spin up coro's for inbound and outbound communication over the socket
+    await asyncio.gather(ws_send_moderator(websocket, chat_info))
+
+
+@app.get("/")
+async def get(request: Request):
+    return templates.TemplateResponse("chat.html",
+                                      {"request": request,
+                                       "ip": get_local_ip(),
+                                       "port": PORT})
+
+
+@app.get("/moderator")
+async def get(request: Request):
+    return templates.TemplateResponse("moderator_chat.html",
+                                      {"request": request,
+                                       "ip": get_local_ip(),
+                                       "port": PORT})
+
+
+async def verify_user_for_room(chat_info):
+    verified = True
+    pool = await get_redis_pool()
+    if not pool:
+        print('Redis connection failure')
+        return False
+    # check for duplicated user names
+    already_exists = await pool.sismember(chat_info['room'] + ":users", chat_info['username'])
+    if already_exists:
+        verified = False
+    # check for restricted names
+
+    # check for restricted rooms
+    # check for non existent rooms
+    # whitelist rooms
+    if not chat_info['room'] in ALLOWED_ROOMS:
+        verified = False
+    pool.close()
+    return verified
 
 
 if __name__ == "__main__":
