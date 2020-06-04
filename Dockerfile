f7979d0bc1094079c88f62ea64745dbaddb6b57c
leon <monstermovie@gmail.com>
2020-06-04 23:07:04 +0100

    use secure address for websocket connection

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3.8-alpine
 WORKDIR /code
 # ENV FLASK_APP app.py
-RUN apk add --no-cache gcc musl-dev linux-headers make
+RUN apk add --no-cache gcc musl-dev linux-headers make python3-dev openssl-dev libffi-dev git
 COPY requirements.txt requirements.txt
 RUN pip install -U setuptools pip
-RUN pip install -r requirements.txt
+# docker overwrites the src location for editable packages so we pass in a --src path that doesnt get blatted
+# https://stackoverflow.com/questions/29905909/pip-install-e-packages-dont-appear-in-docker
+RUN pip install -r requirements.txt --src /usr/local/src
 COPY . .
 CMD ["python", "chat.py"]
--- chat.py
+++ chat.py
@@ -1,29 +1,51 @@
+import os
 import asyncio
 import aioredis
 import uvloop
 import socket
-from fastapi import FastAPI, Depends
-from starlette.requests import Request
+import uuid
+import contextvars
+from fastapi import FastAPI, Depends, Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.websockets import WebSocket, WebSocketDisconnect
+
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
 from aioredis.errors import ConnectionClosedError as ServerConnectionClosedError
 
-REDIS_HOST = 'redis'
+REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 XREAD_TIMEOUT = 0
 XREAD_COUNT = 100
 NUM_PREVIOUS = 30
 STREAM_MAX_LEN = 1000
-ALLOWED_ROOMS = ['chat:1', 'chat:2', 'chat:3', 'leonfeed']
-PORT = 8080
+ALLOWED_ROOMS = ['chat:1', 'chat:2', 'chat:3']
+PORT = 9080
 HOST = "0.0.0.0"
+
+cvar_client_addr = contextvars.ContextVar('client_addr', default=None)
+cvar_chat_info = contextvars.ContextVar('chat_info', default=None)
+cvar_tenant = contextvars.ContextVar('tenant', default=None)
+cvar_redis = contextvars.ContextVar('redis', default=None)
+
+
+class CustomHeaderMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, header_value='Example'):
+        print('__init__')
+        super().__init__(app)
+        self.header_value = header_value
+
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers['Custom'] = self.header_value
+        return response
+
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(CustomHeaderMiddleware)
 templates = Jinja2Templates(directory="templates")
 
 
@@ -32,6 +54,8 @@ def get_local_ip():
     copy and paste from
     https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
     """
+    if os.environ.get('CHAT_HOST_IP', False):
+        return os.environ['CHAT_HOST_IP']
     try:
         ip = [l for l in (
             [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if
@@ -44,7 +68,7 @@ def get_local_ip():
     except OSError as e:
         print(e)
         return '127.0.0.1'
-    print(ip)
+
     return ip
 
 
@@ -115,7 +139,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
             if first_run:
                 # fetch some previous chat history
                 events = await pool.xrevrange(
-                    stream=chat_info['room'],
+                    stream=cvar_tenant.get() + ":stream",
                     count=NUM_PREVIOUS,
                     start='+',
                     stop='-'
@@ -127,7 +151,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                     await websocket.send_json(e)
             else:
                 events = await pool.xread(
-                    streams=[chat_info['room']],
+                    streams=[cvar_tenant.get() + ":stream"],
                     count=XREAD_COUNT,
                     timeout=XREAD_TIMEOUT,
                     latest_ids=latest_ids
@@ -136,6 +160,7 @@ async def ws_send(websocket: WebSocket, chat_info: dict):
                     e['e_id'] = e_id
                     await websocket.send_json(e)
                     latest_ids = [e_id]
+            #print('################contextvar ', cvar_tenant.get())
         except ConnectionClosedError:
             ws_connected = False
 
@@ -158,6 +183,7 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
     :param chat_info:
     :type chat_info:
     """
+
     ws_connected = False
     pool = await get_redis_pool()
     added = await add_room_user(chat_info, pool)
@@ -171,16 +197,20 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
     while ws_connected:
         try:
             data = await websocket.receive_json()
+            #print(data)
+            if type(data) == list and len(data):
+                data = data[0]
             fields = {
                 'uname': chat_info['username'],
                 'msg': data['msg'],
                 'type': 'comment',
                 'room': chat_info['room']
             }
-            await pool.xadd(stream=chat_info['room'],
+            await pool.xadd(stream=cvar_tenant.get() + ":stream",
                             fields=fields,
                             message_id=b'*',
                             max_len=STREAM_MAX_LEN)
+            #print('################contextvar ', cvar_tenant.get())
         except WebSocketDisconnect:
             await remove_room_user(chat_info, pool)
             await announce(pool, chat_info, 'disconnected')
@@ -198,18 +228,21 @@ async def ws_recieve(websocket: WebSocket, chat_info: dict):
 
 
 async def add_room_user(chat_info: dict, pool):
-    added = await pool.sadd(chat_info['room']+":users", chat_info['username'])
+    #added = await pool.sadd(chat_info['room']+":users", chat_info['username'])
+    added = await pool.sadd(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
     return added
 
 
 async def remove_room_user(chat_info: dict, pool):
-    removed = await pool.srem(chat_info['room']+":users", chat_info['username'])
+    #removed = await pool.srem(chat_info['room']+":users", chat_info['username'])
+    removed = await pool.srem(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
     return removed
 
 
 async def room_users(chat_info: dict, pool):
-    users = await pool.smembers(chat_info['room']+":users")
-    #print(users)
+    #users = await pool.smembers(chat_info['room']+":users")
+    users = await pool.smembers(cvar_tenant.get()+":users")
+    print(len(users))
     return users
 
 
@@ -227,7 +260,7 @@ async def announce(pool, chat_info: dict, action: str):
     }
     #print(fields)
 
-    await pool.xadd(stream=chat_info['room'],
+    await pool.xadd(stream=cvar_tenant.get() + ":stream",
                     fields=fields,
                     message_id=b'*',
                     max_len=STREAM_MAX_LEN)
@@ -241,12 +274,21 @@ async def chat_info_vars(username: str = None, room: str = None):
     :param room:
     :type room:
     """
+    if username is None and room is None:
+        return {"username": str(uuid.uuid4()), "room": 'chat:1'}
     return {"username": username, "room": room}
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket,
                              chat_info: dict = Depends(chat_info_vars)):
+    #print('request.hostname', websocket.url.hostname)
+    tenant_id = ":".join([websocket.url.hostname.replace('.', '_'),
+                          chat_info['room']])
+    cvar_tenant.set(tenant_id)
+    cvar_chat_info.set(chat_info)
+
+
     # check the user is allowed into the chat room
     verified = await verify_user_for_room(chat_info)
     # open connection
@@ -257,6 +299,7 @@ async def websocket_endpoint(websocket: WebSocket,
         print(chat_info)
         await websocket.close()
     else:
+
         # spin up coro's for inbound and outbound communication over the socket
         await asyncio.gather(ws_recieve(websocket, chat_info),
                              ws_send(websocket, chat_info))
@@ -302,8 +345,9 @@ async def verify_user_for_room(chat_info):
         print('Redis connection failure')
         return False
     # check for duplicated user names
-    already_exists = await pool.sismember(chat_info['room'] + ":users", chat_info['username'])
+    already_exists = await pool.sismember(cvar_tenant.get()+":users", cvar_chat_info.get()['username'])
     if already_exists:
+        print(chat_info['username'] +' user already_exists in ' + chat_info['room'])
         verified = False
     # check for restricted names
 
@@ -316,7 +360,28 @@ async def verify_user_for_room(chat_info):
     return verified
 
 
+@app.on_event("startup")
+async def handle_startup():
+    try:
+        pool = await aioredis.create_redis_pool(
+            (REDIS_HOST, REDIS_PORT), encoding='utf-8', maxsize=20)
+        cvar_redis.set(pool)
+        print("Connected to Redis on ", REDIS_HOST, REDIS_PORT)
+    except ConnectionRefusedError as e:
+        print('cannot connect to redis on:', REDIS_HOST, REDIS_PORT)
+        return
+
+
+@app.on_event("shutdown")
+async def handle_shutdown():
+    redis = cvar_redis.get()
+    redis.close()
+    await redis.wait_closed()
+    print("closed connection Redis on ", REDIS_HOST, REDIS_PORT)
+
+
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host=HOST, port=PORT)
+    print(dir(app))
+    print(app.url_path_for('websocket_endpoint'))
+    uvicorn.run('chat:app', host=HOST, port=PORT, log_level='info', reload=True)#, uds='uvicorn.sock')
